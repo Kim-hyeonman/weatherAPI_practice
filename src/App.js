d583503bb0a77ebacef9468a34bc98b721d35e9f
hyeonman1108 <hyeonman1108@naver.com>
2024-07-09 17:07:52 +0900

    날씨 api 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [weatherData, setWeatherData] = useState(null);
+  const [location, setLocation] = useState({ lat: null, lon: null });
+  const API_KEY = '467c0e39f1b656dea83929e8f1462fea';
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      setLocation({ lat, lon });
+    });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setWeatherData(data);
+    console.log('Weather Data:', data);
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  useEffect(() => {
+    if (location.lat && location.lon) {
+      getWeatherByCurrentLocation(location.lat, location.lon);
+    }
+  }, [location]);
+
+  return;
 }
 
 export default App;
